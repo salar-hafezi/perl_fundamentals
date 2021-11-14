4f5ea42978e5fc017e2071c459bc81f0c26d2efe
Salar Hafezi <salar.hfz@gmail.com>
2021-11-14 11:30:26 +0330

    ch01

---
+++ src/hello_world.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print "Hello World!\n";
+print 'Hello', ' ', 'World', "\n";
+
+warn "This is just to let you know\n";
+
+die "Something went wrong\n";
+# exit 0;
